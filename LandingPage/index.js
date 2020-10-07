3d77e386ad01ebb62f5df0633807b3da2ec5216a
Danilpaschenko802 <Danilpaschenko802@gmail.com>
2020-10-07 11:02:41 +0300

    first site frame

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -2,8 +2,8 @@ const file = document;
 console.log(file);
 const body = document.querySelector('body');
 console.log(body);
-const item = document.querySelector('navList');
+const item = document.querySelector('.navList');
 console.log(item);
-nLItem.addEventListener("click", () => {
-body.style.color = 'black'
-});
+// nLItem.addEventListener("click", () => {
+// body.style.color = 'black'
+// });
